094f72111b4855907b6365e2d5dc27c6b46b0ec8
Ryan Layer <ryan.layer@gmail.com>
2016-04-06 10:00:11 -0600

    pick samples

--- sv_depth.py
+++ sv_depth.py
@@ -1,19 +1,19 @@
-#!/bin/env python
+#!/usr/bin/env python
 import sys
 import subprocess
 import pysam
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     sys.stderr.write('usage:\t' + \
                      sys.argv[0] + \
-                     ' <ped> <padding (+-500)\n')
+                     ' <ped> <padding (e.g. +-500)> <sample ids (opt)>\n')
     sys.exit(1)
 
 ped_file_name = sys.argv[1]
-
-padding = 500
-if len(sys.argv) == 3:
-    padding = int(sys.argv[2])
+padding = int(sys.argv[2])
+samples = []
+if len(sys.argv) == 4:
+    samples = sys.argv[3:]
 
 f = open(ped_file_name, 'r')
 
@@ -22,7 +22,8 @@ for l in f:
     A = l.rstrip().split('\t')
     if A[0] == 'family_id':
         continue
-    sample_to_bam[A[1]] = [A[-1],0]
+    if (len(samples) == 0)  or (A[1] in samples):
+        sample_to_bam[A[1]] = [A[-1],0]
 f.close()
 
 infile = pysam.VariantFile("-", "r")
@@ -34,8 +35,10 @@ for sample in infile.header.samples:
     i += 1
 
 for variant in infile:
-    if variant.info['SVTYPE'] != 'DEL':
+    if (variant.info['SVTYPE'] != 'DEL')  and (variant.info['SVTYPE'] != 'DUP'):
         continue
+
+    print 'var_' + variant.id + '.txt'
     f = open('var_' + variant.id + '.txt', 'w')
     #print variant.info['SVTYPE'], variant.chrom, variant.start, variant.stop
     f.write(' '.join([str(x) for x in[variant.info['SVTYPE'],\
@@ -46,12 +49,13 @@ for variant in infile:
     for sample in sample_to_bam:
         gt_raw = variant.samples[sample_to_bam[sample][1]].allele_indices
         gt = '/'.join([str(x) for x in gt_raw])
-        cmd = "samtools mpileup -r " + \
+        cmd = "samtools depth -r " + \
                variant.chrom + ":" + \
                     str(max(0,variant.start-padding)) + "-" + \
                     str(variant.stop+padding) + " " + \
               sample_to_bam[sample][0] + \
-              "| cut -f2,4"
+              "| cut -f2,3"
+              #"| cut -f2,4"
               #"| tr '\\n' ' '"
         #cmd = ["ls", "-l", sample_to_bam[sample][0] ]
         #print cmd
